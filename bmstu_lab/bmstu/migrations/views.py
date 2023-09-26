d5b8f63df43baf59599c9ce50698c1d3e465ef3d
lekswell <leks.kuzmin29@mail.ru>
2023-09-26 23:14:24 +0000

    Вернул всё на место

--- bmstu_lab/bmstu/migrations/views.py
+++ bmstu_lab/bmstu/migrations/views.py
@@ -1,85 +1,40 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from datetime import date
+from bmstu.models import Events
+from django.db.models import Q
+from django.shortcuts import render, get_object_or_404
 
 # Мероприятия музея МГТУ. Услуги - виды проводимых в музее мероприятий, 
 # заявки - заявки для групп на данные мероприятия
 
-# def hello(request):
-#     return render(request, 'index.html', { 'data' : {
-#         'current_date': date.today(),
-#         'list': ['python', 'django', 'html']
-#     }})
-
-# def GetOrders(request):
-#     return render(request, 'orders.html', {'data' : {
-#         'current_date': date.today(),
-#         'orders': [
-#             {'title': 'Книга с картинками', 'id': 1},
-#             {'title': 'Бутылка с водой', 'id': 2},
-#             {'title': 'Коврик для мышки', 'id': 3},
-#         ]
-#     }})
-
-# def GetOrder(request, id):
-#     return render(request, 'order.html', {'data' : {
-#         'current_date': date.today(),
-#         'id': id
-#     }})
-
-from datetime import datetime
-
 def GetEvents(request):
-    events = [
-        {'title': 'В.И.Гриневецкий', 'id': 1, 'image': 'static/images/event1.jpg', 'dates': '10-15 сентября 2023'},
-        {'title': 'Лыжная одиссея', 'id': 2, 'image': 'static/images/event2.jpg', 'dates': '15-20 сентября 2023'},
-        {'title': 'В.П.Бармин', 'id': 3, 'image': 'static/images/event3.jpg', 'dates': '20-25 сентября 2023'},
-        {'title': 'Воспитательный дом', 'id': 4, 'image': 'static/images/event4.jpg', 'dates': '25-30 сентября 2023'}
-    ]
-    
     # Получаем ключевое слово из запроса GET
     keyword = request.GET.get('query', '')
 
     # Фильтруем мероприятия по ключевому слову или по дате
-    filtered_events = []
-
-    for event in events:
-        if keyword.lower() in event['title'].lower() or keyword.lower() in event['dates'].lower():
-            filtered_events.append(event)
+    filtered_events = Events.objects.filter(
+        Q(Name__icontains=keyword) | Q(Start_date__icontains=keyword) | Q(End_date__icontains=keyword)
+    )
 
     return render(request, 'events.html', {'data': {
         'current_date': date.today(),
-        'events': filtered_events,  # Передаем отфильтрованные мероприятия
+        'events': filtered_events.values('Event_id', 'Name', 'Start_date', 'End_date', 'Image', 'Status'),  
+        # Передаем отфильтрованные мероприятия, исключая поле Info
         'keyword': keyword,  # Передаем ключевое слово для отображения на странице
     }})
 
 
-
 def GetEvent(request, eventId):
-    events = [
-        {'title': 'В.И.Гриневецкий', 'id': 1, 'image': '/static/images/event1.jpg', 'dates': '10-15 сентября 2023'},
-        {'title': 'Лыжная одиссея', 'id': 2, 'image': '/static/images/event2.jpg', 'dates': '15-20 сентября 2023'},
-        {'title': 'В.П.Бармин', 'id': 3, 'image': '/static/images/event3.jpg', 'dates': '20-25 сентября 2023'},
-        {'title': 'Воспитательный дом', 'id': 4, 'image': '/static/images/event4.jpg', 'dates': '25-30 сентября 2023'}
-    ]
     
-    Event = None
+    # Event = None
+    event = get_object_or_404(Events, Event_id=eventId)
+    # for event in events:
+    #     if event['id'] == eventId:
+    #         Event = event 
     
-    for event in events:
-        if event['id'] == eventId:
-            Event = event 
-            break
-    if Event is None:
-        return HttpResponse('Информация не найдена')
-    print (Event)
     return render(request, 'event.html', {'data': {
-    'title': Event['title'],  # Обратитесь к данным через ключ 'title'
-    'image': Event['image'],  # Обратитесь к данным через ключ 'image'
-    'dates': Event['dates'],  # Обратитесь к данным через ключ 'dates'
-}})
-
-
-
-def sendText(request):
-    input_text = request.POST['text']
-    ...
+        'title': event.Name,    # Обратитесь к полю Name модели Events
+        'image': event.Image,   # Обратитесь к полю Image модели Events
+        'dates': f'{event.Start_date} - {event.End_date}',  # Обратитесь к полям Start_date и End_date
+        'info': event.Info,     # Обратитесь к полю Info модели Events
+    }})
